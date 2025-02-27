000c156a310249f80d67a3f2e2b47c24017e8ce9
Nakanishi Shoichiro <sheepwing@kyudai.jp>
2025-02-27 16:13:04 +0900

    Export speed up
    Just speed up.

--- ntags/commands/io.py
+++ ntags/commands/io.py
@@ -1,23 +1,32 @@
-from glob import glob
 from ..lib.dbclass import DataBase, get_inode, check_tagdb, DEFAULT_TAGDB_FNAME
-from ..lib.ninpipe import Pipe
 from sys import stdin
 import json
+from multiprocessing import Pool
+import os
+from itertools import chain
+
+
+def _get_inode_fname(data):
+    paths = [os.path.join(data[0], p) for p in data[1]]\
+        + [os.path.join(data[0], p) for p in data[2]]
+    return [(get_inode(path), path) for path in paths]
+
 
 def export_command():
-    flist = list(glob('**', recursive=True))
-    result = {}
     fname_dict = {}
-    tag_dict = {}
     with DataBase(check_tagdb(DEFAULT_TAGDB_FNAME)) as db:
-        for fname in flist:
-            tags = db.inode2tag(get_inode(fname))
+        with Pool(min(os.cpu_count(), 8)) as p:
+            inode_file_list = p.map(_get_inode_fname, os.walk('.'))
+        for inode, fname in chain.from_iterable(inode_file_list):
+            tags = db.inode2tag(inode)
             for tag in tags:
                 if tag[0] in fname_dict:
                     fname_dict[tag[0]].append(fname)
                 else:
                     fname_dict.update({tag[0]: [fname]})
-        print(json.dumps({'tags': list(db.get_taglist()), 'files': fname_dict}))
+        print(json.dumps(
+            {'tags': list(db.get_taglist()), 'files': fname_dict}
+        ))
 
 
 def import_command():
@@ -29,5 +38,4 @@ def import_command():
             db.make_new_tag(key, value)
         for key, values in fname_dict.items():
             for value in values:
-                with DataBase(check_tagdb(DEFAULT_TAGDB_FNAME)) as db:
-                    db.add_tag(get_inode(value), key)
+                db.add_tag(get_inode(value), key)
