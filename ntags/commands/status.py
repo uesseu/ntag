ddde94817ac4b77cea68b2d980e52a0598267914
Nakanishi Shoichiro <sheepwing@kyudai.jp>
2026-06-09 13:39:30 +0900

    bugfix
    argument parser

--- ntags/commands/status.py
+++ ntags/commands/status.py
@@ -4,9 +4,17 @@ from ..lib.dbclass import (
     print_status, DataBase,
     DEFAULT_TAGDB_FNAME
 )
+from ..lib.misc import set_custom_directory
+from argparse import ArgumentParser
 
 
 def status_command():
+    parser = ArgumentParser(
+        usage='''Show status of ntag.'''
+    )
+    parser.add_argument('command')
+    set_custom_directory(parser)
+    args = parser.parse_args()
     print_status(
         DataBase(DEFAULT_TAGDB_FNAME, args.directory if args.relative else '')
     )
